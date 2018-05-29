76e8a09a7c00305b070e6688dbbff3cb9e675e45
takumi.hashimoto <ec2-user@ip-172-31-14-114.us-east-2.compute.internal>
2018-05-29 05:23:24 +0000

    fixed issues from the previous commit.

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -5,7 +5,7 @@
     <h1>Details of id = {{ $task->id }}</h1>
 
     <p>Content: {{ $task->content }}</p>
-    <p>Status: {{ $task->content }}</p>
+    <p>Status: {{ $task->status }}</p>
     
     {!! link_to_route('tasks.edit', 'Edit this task.', ['id' => $task->id]) !!}
     {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
