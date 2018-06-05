a0548937b8d982c1920e106517877e641a21f5af
takumi.hashimoto <ec2-user@ip-172-31-14-114.us-east-2.compute.internal>
2018-06-05 04:50:48 +0000

    user pages

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-    <h1>Details of id = {{ $task->id }}</h1>
+    <h1>Details of No.{{ $task->id }}</h1>
     <table class="table table-bordered">
         <tr>
             <th>id</th>
@@ -17,7 +17,7 @@
             <td>{{ $task->status }}</td>
         </tr>
     </table>
-     {!! link_to_route('tasks.edit', 'Edit this task', ['id' => $task->id], ['class' => 'btn btn-default']) !!}
+     {!! link_to_route('tasks.edit', 'Edit', ['id' => $task->id], ['class' => 'btn btn-default']) !!}
 
     {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
         {!! Form::submit('Delete', ['class' => 'btn btn-danger']) !!}
