31f374fc70b29eab5780853fda1de1bfade7501c
takumi.hashimoto <ec2-user@ip-172-31-14-114.us-east-2.compute.internal>
2018-06-06 01:58:11 +0000

    tasklist login

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -14,12 +14,12 @@
                 <ul class="nav navbar-nav navbar-right">
                     @if (Auth::check())
                         <li>{!! link_to_route('tasks.create', 'Add Task') !!}</li>
-                        <li>{!! link_to_route('users.index', 'Users') !!}</li>
+                        {{--<li>{!! link_to_route('users.index', 'Users') !!}</li>--}}
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
                             <ul class="dropdown-menu">
-                                <li><a href="#">My profile</a></li>
-                                <li role="separator" class="divider"></li>
+                                {{--<li><a href="#">My profile</a></li>
+                                <li role="separator" class="divider"></li>--}}
                                 <li>{!! link_to_route('logout.get', 'Logout') !!}</li>
                             </ul>
                         </li>
--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-
+    @if (Auth::user()->id == $task->user_id)
     <h1>Details of No.{{ $task->id }}</h1>
     <table class="table table-bordered">
         <tr>
@@ -18,11 +18,12 @@
         </tr>
     </table>
      {!! link_to_route('tasks.edit', 'Edit', ['id' => $task->id], ['class' => 'btn btn-default']) !!}
-     
-    @if (Auth::user()->id == $task->user_id)
         {!! Form::open(['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
         {!! Form::submit('Delete', ['class' => 'btn btn-danger btn-xs']) !!}
         {!! Form::close() !!}
+        
+    @else
+        {{ print('<h2 class="alert alert-danger"><span class="glyphicon glyphicon-ban-circle" aria-hidden="true"></span>You are not authorized for that action!</h2>')}}
     @endif
 
 @endsection
