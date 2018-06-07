a2566304efe5bd5fae08416391a54b0c6c391ba8
takumi.hashimoto <ec2-user@ip-172-31-14-114.us-east-2.compute.internal>
2018-06-07 00:22:23 +0000

    added auth check on show, create

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-
+    @if (Auth::user()->id == $task->user_id)
     <h1>Update Task No. id: {{ $task->id }}</h1>
     <div class="row">
         <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-lg-6 col-lg-offset-3">
@@ -22,4 +22,8 @@
             {!! Form::close() !!}
         </div>
     </div>
+    @else
+        {{ print('<h2 class="alert alert-danger"><span class="glyphicon glyphicon-ban-circle" aria-hidden="true"></span>You are not authorized for that action!</h2>')}}
+    @endif
+
 @endsection
